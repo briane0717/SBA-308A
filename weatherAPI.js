105047621ea00125b613b33b81ad518e456582a8
Brian <briane0717@gmail.com>
2024-11-25 23:50:23 -0500

    linked html elements in the dom

--- weatherAPI.js
+++ weatherAPI.js
@@ -26,7 +26,36 @@ async function getWeatherData(city) {
   return await response.json();
 }
 
-function displayWeatherInfo(data) {}
+function displayWeatherData(data) {
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ description, id }],
+  } = data;
+  card.textContent = "";
+  card.style.display = "flex";
+  const cityDisplay = document.createElement("h1");
+  const tempDisplay = document.createElement("p");
+  const humidityDisplay = document.createElement("p");
+  const descriptionDisplay = document.createElement("p");
+  const weatherEmoji = document.createElement("p");
+  cityDisplay.textContent = city;
+  tempDisplay.textContent = `${((temp - 273.15) * (9 / 5) + 32).toFixed(1)}°F`;
+  humidityDisplay.textContent = `${humidity}% humidity`;
+  descriptionDisplay.textContent = `${description}`;
+  weatherEmoji.textContent = getWeatherEmoji(id);
+
+  cityDisplay.classList.add("cityDisplay");
+  tempDisplay.classList.add("tempDisplay");
+  humidityDisplay.classList.add("humidityDisplay");
+  descriptionDisplay.classList.add("descriptionDisplay");
+  weatherEmoji.classList.add("weatherEmoji");
+  card.appendChild(cityDisplay);
+  card.appendChild(tempDisplay);
+  card.appendChild(humidityDisplay);
+  card.appendChild(descriptionDisplay);
+  card.appendChild(weatherEmoji);
+}
 
 function getWeatherEmoji(weatherId) {}
 
