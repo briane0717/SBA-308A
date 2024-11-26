178c67b623e6a98d477f80280dee653affbad890
Brian <briane0717@gmail.com>
2024-11-25 20:01:59 -0500

    started over

--- api.js
+++
@@ -1,6 +0,0 @@
-async function initialLoad() {
-  const response = await fetch("https://opentdb.com/api.php?amount=10");
-  const questions = await response.json();
-  console.log(questions);
-}
-initialLoad();
