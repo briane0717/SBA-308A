23e946ec0a706a90814b8a710c795606340fc809
Brian <briane0717@gmail.com>
2024-11-25 23:47:51 -0500

    added logic to callback function

--- weatherAPI.js
+++ weatherAPI.js
@@ -2,12 +2,18 @@ const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "f2a6c531b1f2d7fb41e1b03ee4c8c527";
-weatherForm.addEventListener("submit", (event) => {
+weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   const city = cityInput.value;
   if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      displayError("Could not fetch weather data");
+    }
   } else {
-    displayError("Please enter a city.");
+    displayError("Please enter a city");
   }
 });
 
