8e9e89addb75df25c455d050db2826435260d356
Brian <briane0717@gmail.com>
2024-11-25 23:44:54 -0500

    added all functions needed

--- weatherAPI.js
+++ weatherAPI.js
@@ -5,5 +5,23 @@ const apiKey = "f2a6c531b1f2d7fb41e1b03ee4c8c527";
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const city = cityInput.value;
-  getWeatherData(city);
+  if(city){
+    else{
+      displayError("Please enter a city");
+    }
+  }
+ 
 });
+
+async function getWeatherData(city) {}
+
+function displayWeatherInfo(data) {}
+
+function getWeatherEmoji(weatherId){}
+
+
+
+function displayError(message) {
+   errorDisplay.textContent = message;
+}
+
